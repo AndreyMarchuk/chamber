d001a2c1086c3a303c6939b8f009d69e41624fe1
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2018-08-29 20:15:09 +0200

    Add tests for validations

---
+++ cmd/root_test.go
@@ -0,0 +1,100 @@
+package cmd
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidations(t *testing.T) {
+
+	// Test Key formats
+	validKeyFormat := []string{
+		"foo",
+		"foo.bar",
+		"foo.",
+		".foo",
+	}
+
+	for _, k := range validKeyFormat {
+		t.Run("Key validation should return Nil", func(t *testing.T) {
+			result := validateKey(k)
+			assert.Nil(t, result)
+		})
+	}
+
+	invalidKeyFormat := []string{
+		"/foo",
+		"foo//bar",
+		"foo/bar",
+	}
+
+	for _, k := range invalidKeyFormat {
+		t.Run("Key validation should return Error", func(t *testing.T) {
+			result := validateKey(k)
+			assert.Error(t, result)
+		})
+	}
+
+	// Test Service format without PATH
+	os.Setenv("CHAMBER_NO_PATHS", "true")
+	validServiceNoPathFormat := []string{
+		"foo",
+		"foo.bar",
+		"foo.bar.foo",
+	}
+
+	for _, k := range validServiceNoPathFormat {
+		t.Run("Service without PATH validation should return Nil", func(t *testing.T) {
+			result := validateService(k)
+			assert.Nil(t, result)
+		})
+	}
+
+	invalidServiceNoPathFormat := []string{
+		"foo.",
+		".foo",
+		"/foo",
+		"foo//bar",
+		"foo/bar",
+	}
+
+	for _, k := range invalidServiceNoPathFormat {
+		t.Run("Service without PATH validation should return Nil", func(t *testing.T) {
+			result := validateService(k)
+			assert.Error(t, result)
+		})
+	}
+	os.Unsetenv("CHAMBER_NO_PATHS")
+
+	// Test Service format with PATH
+	validServicePathFormat := []string{
+		"foo",
+		"foo/bar",
+		"foo/bar/foo",
+	}
+
+	for _, k := range validServicePathFormat {
+		t.Run("Service with PATH validation should return Nil", func(t *testing.T) {
+			result := validateService(k)
+			assert.Nil(t, result)
+		})
+	}
+
+	invalidServicePathFormat := []string{
+		"foo/",
+		"foo.bar",
+		"foo.",
+		".foo",
+		"/foo",
+		"foo//bar",
+	}
+
+	for _, k := range invalidServicePathFormat {
+		t.Run("Service with PATH validation should return Nil", func(t *testing.T) {
+			result := validateService(k)
+			assert.Error(t, result)
+		})
+	}
+}
